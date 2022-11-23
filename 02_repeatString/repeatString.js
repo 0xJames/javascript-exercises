4d0d78981793ef329c81f7fe3b27fa3149f9e7bd
James Yang <jy0xcs@gmail.com>
2022-11-23 13:23:16 -0800

    Finish exercise "repeatString"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,num) {
+    let repeatedstr = ""
+      if (num >= 0) { 
+        while (num > 0){
+            repeatedstr = repeatedstr + string;
+            num--;
+            
+        }
+        return repeatedstr
+      }
+        else{
+          return "ERROR"
+        }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
