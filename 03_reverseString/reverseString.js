c34abd53ef2f7e1b9256f9249427bf2380b83280
James Yang <jy0xcs@gmail.com>
2022-11-24 08:37:17 -0800

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
 
-};
+const reverseString = function(word) {
+    let chararray = []
+    var stringleng = word.length
+    for (let i = 0; i < stringleng; i++){
+        let char =""
+        char = word.slice(i,i+1)
+        chararray.push(char)
+    }
+    
+    var reverseword =""
+    for (i=1;i <= chararray.length; i++){
+        reverseword = reverseword + chararray[chararray.length-i]
+    }
+    return reverseword;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
