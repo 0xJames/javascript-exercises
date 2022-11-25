8e9defd90af0d2e002a271148fd8aeb214c449ce
James Yang <jy0xcs@gmail.com>
2022-11-24 21:47:15 -0800

    Complete exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0){
+        return "ERROR"
+    }
+        else if (Number.isInteger(x) == false || Number.isInteger(y)== false){
+            return "ERROR"
+        }
+        else{sum = (x+y) * (Math.abs(y-x)+1) * 0.5;
+        }
+    return sum;
 
 };
 
