dc388c179bc996eb7fbc3ad3328e87f5c59f64d4
James Yang <jy0xcs@gmail.com>
2022-11-25 13:39:41 -0800

    Complete leap year exercise.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year % 400 == 0){
+        return true //remaining numbers: not divisible by 400
+    }
+        else if(year % 100 ==0){
+            return false   //remaining numbers: not divisible by 100
+        }
+        else if(year % 4 ==0){
+            return true  //remaining numbers: not divisible by 4
+        }
+        else {
+            return false;
+        }
+   
 
 };
 
