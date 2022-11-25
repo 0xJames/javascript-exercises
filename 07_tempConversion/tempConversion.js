b7f517eed5a076671ecf92687f76879f40dfa611
James Yang <jy0xcs@gmail.com>
2022-11-25 14:38:28 -0800

    Complete exercise with different method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  degCel= (degreeF- 32) * 5/9
+  return Number(degCel.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  degFah= (degreeC * 9/5) + 32 
+  return Number(degFah.toFixed(1))
 };
 
 // Do not edit below this line
