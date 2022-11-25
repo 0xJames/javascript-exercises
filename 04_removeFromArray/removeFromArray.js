07153c62bc272a96a0f2386f0595ca434d9ba9a4
James Yang <jy0xcs@gmail.com>
2022-11-24 17:16:23 -0800

    complete removeFromArray exercise; used subtraction here, but addition method is available and is official solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array,...unwanted) {
+    for (items of unwanted){
+        ///console.log(unwanted)
+        index = array.indexOf(items)
+        if (index == -1){
+            continue;
+        }
+            else{
+        ///console.log(index)
+                array.splice(index,1)
+            }
+        
+    }
+    return array
+    
 };
 
 // Do not edit below this line
