45ad38d7d09083838672c604851bb059e6d706f6
James Yang <jy0xcs@gmail.com>
2023-07-09 21:10:51 -0700

    Finish exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    const fibArray = [1,1];
+    let i = 2;
+    if (index <= 0){
+        return "OOPS"
+    } else{
+        while (i<= index){ 
+            fibArray.push(fibArray[i-2]+fibArray[i-1]);
+            i++;
+        }
+        return fibArray[index-1];
+
+    }
+    
 
 };
 
