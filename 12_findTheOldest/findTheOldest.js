cffe6a6365617382e125922bb439916582a657bd
James Yang <jy0xcs@gmail.com>
2023-07-18 15:27:04 -0700

    Finish exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArray) {
+    const date = new Date()
+    const ageArray = peopleArray.map(x => x.yearOfDeath ? x.yearOfDeath - x.yearOfBirth : date.getFullYear()-x.yearOfBirth);
+    const oldest = Math.max(...ageArray);
+    const oldIndex = ageArray.indexOf(oldest);
+    return peopleArray[oldIndex]
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
